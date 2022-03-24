6a79e616441b177170829b293966edd196e4b96b
imubarak234 <imubarak2424@gmail.com>
2022-03-24 17:26:40 +0100

    fix linters

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,37 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users Controller", :type => :request do  
+RSpec.describe 'Users Controller', type: :request do
+  context 'Tests for posts#index' do
+    before(:example) { get '/users/:id/posts' }
 
-  context "Tests for posts#index" do 
-    before(:example) {get "/users/:id/posts"} 
-
-    it "testing correct template rendering" do 
+    it 'testing correct template rendering' do
       expect(response).to render_template('index')
     end
 
-    it "testing correct repsonse status" do 
+    it 'testing correct repsonse status' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "testing response body includes correct placeholder text" do
-      expect(response.body).to include("")
+    it 'testing response body includes correct placeholder text' do
+      expect(response.body).to include('')
     end
   end
 
-  context "Tests for posts#show" do
-    before(:example) {get "/users/:id/posts/:id"} 
+  context 'Tests for posts#show' do
+    before(:example) { get '/users/:id/posts/:id' }
 
-    it "testing correct template rendering" do 
+    it 'testing correct template rendering' do
       expect(response).to render_template('show')
     end
 
-    it "testing correct repsonse status" do 
+    it 'testing correct repsonse status' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "testing response body includes correct placeholder text" do
-      expect(response.body).to include("")
+    it 'testing response body includes correct placeholder text' do
+      expect(response.body).to include('')
     end
   end
-
 end
