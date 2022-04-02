cd63629b1e4c863c53cbe9a508232de25c5e1a63
imubarak234 <imubarak2424@gmail.com>
2022-04-02 23:41:39 +0100

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-  def index
-  end
+  def index; end
 
   def new
     @comment_new = Comment.new
@@ -11,9 +10,9 @@ class CommentsController < ApplicationController
 
   def create
     current_post_id = params[:post_id]
-    current_user_id = params[:user_id] 
+    current_user_id = params[:user_id]
 
-    puts "lllllllllllllllll"
+    puts 'lllllllllllllllll'
     comment = Comment.new(params.require(:comment_new).permit(:text))
     comment.update(author_id: current_user_id)
     comment.update(post_id: current_post_id)
@@ -21,15 +20,15 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html do
         if comment.save
-          flash[:success] = "Comment created successfully"
-          puts "pass pass"
+          flash[:success] = 'Comment created successfully'
+          puts 'pass pass'
           redirect_to user_posts_url
         else
-          flash.now[:error] = "Error: Comment not created"
-          puts "fail lllllllllll"
-          puts "fail lllllllllll"
-          puts "fail lllllllllll"
-          #puts params
+          flash.now[:error] = 'Error: Comment not created'
+          puts 'fail lllllllllll'
+          puts 'fail lllllllllll'
+          puts 'fail lllllllllll'
+          # puts params
           render :new, locals: { posts: @comment_new }
         end
       end
