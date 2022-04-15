c69546fbcd96f3aec7775a5e2b9bafa730a25901
Mubarak <imubarak2424@gmail.com>
2022-04-16 00:28:03 +0100

    post index

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe 'Index Page', type: :feature do
   
   describe 'Testing page functions' do
-    it 'Check for usernames' do
+    it 'Check for CRUD Features' do
       user = User.create(id: 1,name: "tester", photo: "photo.com", bio: "All days", email: "example@email.com",
         password: "123456789")
       visit '/users'
-      expect(page).to have_content 'Username'
-      expect(page).to have_content 'Picture'
-      expect(page).to have_content "Number of posts: #{user.posts_counter}"
+      expect(page).to have_content("#{user.name}", count: User.count)
+      expect(page).to have_content('Picture', count: User.count)
+      expect(page).to have_content("Number of posts: #{user.posts_counter}", count: User.count)
       click_on'Users Page'
      
       expect(page).to have_current_path("/users/#{user.id}")
