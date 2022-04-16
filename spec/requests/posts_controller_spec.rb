b268819cc1231cad93715d1a68d60ea2ffeb4184
Mubarak <imubarak2424@gmail.com>
2022-04-16 02:01:13 +0100

    comment out previous tests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,35 +1,35 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Post Controller', type: :request do
-  context 'Tests for posts#index' do
-    before(:example) { get '/users/1/posts' }
+# RSpec.describe 'Post Controller', type: :request do
+#   context 'Tests for posts#index' do
+#     before(:example) { get '/users/1/posts' }
 
-    it 'testing correct template rendering' do
-      expect(response).to render_template('index')
-    end
+#     it 'testing correct template rendering' do
+#       expect(response).to render_template('index')
+#     end
 
-    it 'testing correct repsonse status' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'testing correct repsonse status' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'testing response body includes correct placeholder text' do
-      expect(response.body).to include('')
-    end
-  end
+#     it 'testing response body includes correct placeholder text' do
+#       expect(response.body).to include('')
+#     end
+#   end
 
-  context 'Tests for posts#show' do
-    before(:example) { get '/users/1/posts/1' }
+#   context 'Tests for posts#show' do
+#     before(:example) { get '/users/1/posts/1' }
 
-    it 'testing correct template rendering' do
-      expect(response).to render_template('show')
-    end
+#     it 'testing correct template rendering' do
+#       expect(response).to render_template('show')
+#     end
 
-    it 'testing correct repsonse status' do
-      expect(response).to have_http_status(:ok)
-    end
+#     it 'testing correct repsonse status' do
+#       expect(response).to have_http_status(:ok)
+#     end
 
-    it 'testing response body includes correct placeholder text' do
-      expect(response.body).to include('')
-    end
-  end
-end
+#     it 'testing response body includes correct placeholder text' do
+#       expect(response.body).to include('')
+#     end
+#   end
+# end
